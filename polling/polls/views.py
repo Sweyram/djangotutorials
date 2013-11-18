29f94fa67432619bccbda0bea0a5a8585482d9cb
Eyram <arrow@envy-testing5.(none)>
2013-11-18 19:37:32 +0000

    Updates on views

--- polling/polls/views.py
+++ polling/polls/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404,HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.shortcuts import render, get_object_or_404
-from polls.models import Poll
+from polls.models import Choice,Poll
 # Create your views here.
 
 def index(request):
@@ -17,4 +17,13 @@ def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+	p = get_object_or_404(Poll, pk=poll_id)
+	try:
+		selected_choice = p.choice_set.get(pk.request.POST['choice'])
+	except(KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/details.html', {'poll':p, 'error_message':'You didn\'t select a choice',})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+    
