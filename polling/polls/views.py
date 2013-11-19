e1fc61b370fa79df07708c439990d86c23394308
Eyram <eyram@eyram-XPS-L501X.(none)>
2013-11-19 00:03:16 +0000

    little updates

--- polling/polls/views.py
+++ polling/polls/views.py
@@ -1,24 +1,27 @@
 from django.http import HttpResponse, Http404,HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from polls.models import Choice,Poll
 # Create your views here.
 
-def index(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    context = {'latest_poll_list': latest_poll_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
 
-def details(request, poll_id):
-    try:
-       poll = Poll.objects.get(pk = poll_id)
-    except Poll.DoesNotExist:
-       raise Http404
-    return render(request, 'polls/detail.html', {'poll':poll})
+    def get_queryset(self):
+        """Return the last five published polls."""
+        return Poll.objects.order_by('-pub_date')[:5]
 
 
-def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
 
 def vote(request, poll_id):
 	p = get_object_or_404(Poll, pk=poll_id)
