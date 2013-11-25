06c15fced15c7250a08035daf64c79cb3d042e0f
Eyram <eyram@eyram-XPS-L501X.(none)>
2013-11-25 01:16:24 +0000

    configured TEMPLATES_DIR

--- tango_with_django_project/tangorango/tangorango/settings.py
+++ tango_with_django_project/tangorango/tangorango/settings.py
@@ -107,7 +107,7 @@ ROOT_URLCONF = 'tangorango.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'tangorango.wsgi.application'
 
-TEMPLATE_DIRS = (
+TEMPLATE_DIRS = ( '/home/eyram/Github/djangotutorials/tango_with_django_project/tangorango/tangorango',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
